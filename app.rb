abe19d25a6c1a1bc10f31f88137c8d0b5eac8cfc
LoganH-R <146144771+LoganH-R@users.noreply.github.com>
2023-10-17 06:53:04 +0000

    Final Product RPS

--- app.rb
+++ app.rb
@@ -10,3 +10,21 @@ BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 get("/") do
   erb(:rules)
 end
+
+get("/rock") do
+  @opponent = ["rock", "paper", "scissors"]
+  @opponent_sample = @opponent.sample
+  erb(:rock)
+end
+
+get("/paper") do
+  @opponent = ["rock", "paper", "scissors"]
+  @opponent_sample = @opponent.sample
+  erb(:paper)
+end
+
+get("/scissors") do
+  @opponent = ["rock", "paper", "scissors"]
+  @opponent_sample = @opponent.sample
+  erb(:scissors)
+end
